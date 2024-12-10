27350ae1438d0468186dd282d5016f8bbe112a6d
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-12-10 21:03:30 +0100

    Buisness Section

--- app/components/Buisness.jsx
+++ app/components/Buisness.jsx
@@ -1,8 +1,20 @@
 const Buisness = () => {
     return (
         <>
-            <section>
-                <h1 className="text-orange-500 text-3xl">Our Buisnesses</h1>
+            <section className="h-screen p-9 bg-black text-white">
+                <h1 className="text-3xl text-center">Our <span className="text-orange-500">Buisnesses</span></h1>
+                <p className="text-center">At Versalink Group, we are dedicated to driving innovation and excellence across various sectors in Nigeria.</p>
+                <section>
+                    <div>
+                        <div>
+                            <img src="realestate.jpg" alt="real estate" />
+                            <h1 className="text-2xl">Versa Homes</h1>
+                        </div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div></div>
+                </section>
             </section>
         </>
     )
