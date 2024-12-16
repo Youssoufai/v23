a9843bb1d30ecc37918ad69dacfb4fbb3785cd98
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-12-16 16:08:54 +0100

    Adding Icons

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,3 +1,4 @@
+import { FaEnvelope } from "react-icons/fa";
 const Contact = () => {
     return (
         <>
@@ -6,6 +7,15 @@ const Contact = () => {
                     <h3 className="text-orange-500">CONTACT US</h3>
                     <h1 className="text-3xl">Get in touch today</h1>
                     <p>We love questions and feedback - and we're always happy to help! <br /> Here are some ways to contact us</p>
+                    <div>
+                        <div>
+                            <FaEnvelope />
+                        </div>
+                        <div>
+                            <h2>Email:</h2>
+                            <h1>contact@email.com</h1>
+                        </div>
+                    </div>
                 </div>
                 <div>
 
