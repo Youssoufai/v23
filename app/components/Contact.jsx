da9abca4ba4943a32b62a90aed3e31f5844269cb
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-12-16 16:40:03 +0100

    Contact Form

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -2,8 +2,8 @@ import { FaEnvelope, FaPhone } from "react-icons/fa";
 const Contact = () => {
     return (
         <>
-            <section className="h-screen flex p-8">
-                <div className="flex flex-col items-start space-y-8">
+            <section className="h-screen flex justify-between p-8">
+                <div className="flex flex-col items-start space-y-8 flex-1">
                     <h3 className="text-orange-500">CONTACT US</h3>
                     <h1 className="text-3xl">Get in touch today</h1>
                     <p className="text-gray-400">We love questions and feedback - and we're always happy to help! <br /> Here are some ways to contact us</p>
@@ -26,13 +26,13 @@ const Contact = () => {
                         </div>
                     </div>
                 </div>
-                <div>
-                    <div>
-                        <form action="#">
+                <div className="bg-slate-400 rounded-sm flex-1">
+                    <form action="#" className="p-9">
+                        <div className="flex flex-col space-y-3">
                             <label for="full_name" className="text-orange-100">Full Name</label>
-                            <input type="text" placeholder="Your name" />
-                        </form>
-                    </div>
+                            <input type="text" className="rounded-sm p-2" placeholder="Your name" />
+                        </div>
+                    </form>
                 </div>
             </section>
         </>
