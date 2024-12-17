4d73952cdaa7443f64e5f0e5d38dee28444d5b00
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-12-17 17:54:08 +0100

    Added Footer

---
+++ app/components/Footer.jsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+    return (
+        <>
+            <section className="h-screen bg-black">
+                <div>
+                    <h1 className="text-center font-bold text-xl">Company</h1>
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+                <div>
+
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Footer
--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Buisness from "./components/Buisness";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Mission from "./components/Mission";
 import Navbar from "./components/Navbar";
@@ -16,6 +17,7 @@ export default function Home() {
       <Team />
       <Buisness />
       <Contact />
+      <Footer />
     </>
   );
 }
