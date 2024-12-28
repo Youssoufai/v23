571bcfbb109d9772b1979c1f575d3a11cfc254e7
Youssoufai <Yusufmuhammadbashir2005@gmail.com>
2024-12-28 14:21:52 +0100

    Added Upcoming section page

---
+++ app/components/Upcoming.jsx
@@ -0,0 +1,20 @@
+const Upcoming = () => {
+    return (
+        <>
+
+            <section className="p-11 h-screen flex space-x-12 gap-12">
+                <div>
+                    <img src="about1.jpg.webp" className="h-[330px] w-[451px]" alt="helpless" />
+                </div>
+                <div className="space-y-4">
+                    <h1 className="">Upcoming Program</h1>
+                    <h1 className="text-4xl text-[#FF6F0F] font-bold">We Help People In Need</h1>
+                    <p>Our mission is to drive positive change by addressing key social, economic, and environmental challenges, ensuring that every individual has the opportunity to thrive.</p>
+                </div>
+            </section>
+
+        </>
+    )
+}
+
+export default Upcoming;
--- app/vdi/page.jsx
+++ app/vdi/page.jsx
@@ -1,16 +1,25 @@
+import Upcoming from "../components/Upcoming";
+
 const Page = () => {
     return (
         <>
 
-            <section className="bg-vdi p-11 h-screen bg-center bg-cover relative" id="vdiBg">
-                <div className="h-full flex items-center">
-                    <div className="flex-1">
-                        <h1 className="text-7xl text-white leading-snug">Give a helping hand to those who need it.</h1>
-                    </div>
-                    <div className="flex-1">
+            <section className="h-full">
+                <section className="bg-vdi p-11 h-screen bg-center bg-cover relative" id="vdiBg">
+                    <div className="h-full flex items-center">
+                        <div className="flex-1 space-y-6">
+                            <h1 className="text-7xl leading-snug text-[#FF6F0F]">Let's Save More Lives With Our Helping Hand</h1>
+                            <p className="text-white">The Versalink Development Initiative (VDI) is the engine of our social impact efforts. This program goes beyond the bottom line, channeling our resources to create positive change.</p>
+                            <button className="px-4 py-3 bg-orange-500 rounded text-white">
+                                Ongoing Programs
+                            </button>
+                        </div>
+                        <div className="flex-1">
 
+                        </div>
                     </div>
-                </div>
+                </section>
+                <Upcoming />
             </section>
         </>
     )
